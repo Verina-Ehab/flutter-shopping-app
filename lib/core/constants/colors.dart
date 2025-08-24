f306a23123754069b06eade431f725dded1b9c59
Verina-Ehab <rinaehab11@gmail.com>
2025-08-24 23:49:58 +0300

    adding core files: - constant - common widgets - helpers - app theme

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -5,12 +5,8 @@ class ColorsManager {
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-  static Color whiteOpacity = Colors.white.withValues(alpha: 0.5);
-  static const Color bckg = Color(0xFF181818);
-  static const Color blackButton = Color(0xFF222627);
-  static const Color blackCard = Color(0xFF1e2020);
+  static const Color bckg = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFF494d4e);
-  static const Color greyOpacity = Color.fromARGB(17, 73, 77, 78);
   static const Color greyIcon = Color(0xFF8c8c8c);
 
   static const Color transparent = Colors.transparent;
@@ -18,13 +14,10 @@ class ColorsManager {
   static const Color green = Color(0xFF1b7d59);
   static const Color orange = Colors.orange;
 
-  static const Color main = Color(0xFF7fcbb6);
-  static const Color second = Color(0xFFd0fe21);
-  static const Color purple = Color(0xFFac02ef);
-  static const Color lightBlue = Color(0xFF1ae6ff);
-  static const Color darkTerqoise = Color(0xff103035);
+  static const Color main = Color(0XFF9333e9);
+  static const Color second = Color(0xFF677de7);
 
   // static const Color topGradient = Color(0xFF264247);
-  static const Color bottomGradient = Color(0xFF181818);
-  static const Color topGradient = Color(0xFF182123);
+  static const Color bottomGradient = Color(0xFF764ea5);
+  static const Color topGradient = Color(0xFF677de7);
 }
--- lib/core/constants/sizes.dart
+++ lib/core/constants/sizes.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 const Size designSize = Size(390, 844);
-EdgeInsets layoutPadding =
-    EdgeInsets.symmetric(horizontal: 16.w, vertical: 18.h);
+EdgeInsets layoutPadding = EdgeInsets.symmetric(
+  horizontal: 16.w,
+  vertical: 18.h,
+);
 
-double appSharedRadius = 4.5.r;
+double appSharedRadius = 12.r;
 double appSharedHorizontalPadding = 16.w;
 
 // double kRadius = 12.r;
